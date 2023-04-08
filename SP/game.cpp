e37980a640d693d7359c892c9ecd04a5368bb324
MazeN Hassn <mazenby6@gmail.com>
2023-04-09 01:45:27 +0200

    Testing

--- SP/game.cpp
+++ SP/game.cpp
@@ -1,24 +1,46 @@
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+RenderWindow window(sf::VideoMode(2560, 1440), "Game", sf::Style::Fullscreen);
+Event event;
+RectangleShape bg;
+Texture bgTexture;
+Sprite bgSprite;
+float bgCounter = 0;
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+void bgSetup() {
+    bgTexture.setRepeated(true);
+    bg.setSize(sf::Vector2f(2560.f, 1440.f));
+    bgTexture.loadFromFile("Textures/BG Sprite Sheet.png");
+    bgSprite.setTexture(bgTexture);
+}
+
+void windowclose() {
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window.close();
+    }
+}
+
+void windowfunction() {
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
+        bgCounter += 0.0075;
+        if (bgCounter > 8)
+            bgCounter -= 8;
+        bgSprite.setTextureRect(IntRect((int)bgCounter * 2560, 0, 2560, 1440));
+        windowclose();
         window.clear();
-        window.draw(shape);
+        window.draw(bgSprite);
         window.display();
     }
+}
 
+int main()
+{
+    bgSetup();
+    windowfunction();
     return 0;
 }
