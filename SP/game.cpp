cc06c3e2b818d3660946c0fb4a5bf08266018a4b
mazin647 <110307117+mazin647@users.noreply.github.com>
2023-04-11 19:41:52 +0200

    blackscreen

--- SP/game.cpp
+++ SP/game.cpp
@@ -1,4 +1,9 @@
 #include <SFML/Graphics.hpp>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 #include <iostream>
 using namespace std;
 using namespace sf;
@@ -8,16 +13,28 @@ RectangleShape player, ground[100];
 Texture bgTexture;
 Sprite bgSprite;
 float bgCounter = 0;
+bool isBlackscreen=false;
+Clock blackscreenTimer;
 View view(Vector2f(0, 0), Vector2f(1920, 1080));
-Mouse ms;
+
+struct Player
+{
+    Texture Playertex;
+    Sprite PlayerSprite;
+}mc;
 
 // DECLRATIONS
+void Playersetup(Player&);
 void bgSetup();
 void windowclose();
 void windowfunction();
+void cameraView();
+bool canMoveRight(RectangleShape,int);
+bool canMoveleft(RectangleShape object,int xPoisition);
 
 int main()
 {
+    Playersetup(mc);
     bgSetup();
     windowfunction();
     return 0;
@@ -25,11 +42,12 @@ int main()
 
 
 //DEFINITIONS
-void bgSetup() 
+void bgSetup()
 {
+    int bgCounter=0;
     player.setSize(Vector2f(50, 50));
     player.setPosition(600, 600);
-    bgTexture.loadFromFile("Level 1-A BG.png");
+    bgTexture.loadFromFile(resourcePath()+"Level 1-A BG.png");
     bgSprite.setTexture(bgTexture);
     bgSprite.setPosition(-370, -53);
 }
@@ -38,28 +56,33 @@ void windowfunction()
 
     while (window.isOpen())
     {
-        if (Keyboard::isKeyPressed(Keyboard::Key::Right) && player.getPosition().x < 9063 - 370 - 50) {
-            player.move(20, 0);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Right) && canMoveRight(player,8643))
+        {
+            player.move(0.25, 0);
         }
-        if (Keyboard::isKeyPressed(Keyboard::Key::Left) && player.getPosition().x > -370) {
-            player.move(-20, 0);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Left) && canMoveleft(player, -370))
+        {
+            player.move(-0.25, 0);
         }
-        if (player.getPosition().x <= 7740 && player.getPosition().x >= 600)
-            view.setCenter(player.getPosition());
-        else if (player.getPosition().x > 7740) {
-            view.setCenter(7741, player.getPosition().y);
-        }
-        else if (player.getPosition().x < 600)
-            view.setCenter(599, player.getPosition().y);
+        cameraView();
         windowclose();
         window.clear();
         window.draw(bgSprite);
         window.draw(player);
+        window.draw(mc.PlayerSprite);
         window.setView(view);
+        if(player.getPosition().x>8633)
+        {
+            window.clear(Color::Black);
+            window.display();
+            player.setPosition(600, 600);
+            sleep(2);
+        }
+        else
         window.display();
     }
 }
-void windowclose() 
+void windowclose()
 {
     while (window.pollEvent(event))
     {
@@ -69,4 +92,40 @@ void windowclose()
             window.close();
         }
     }
+}
+void cameraView()
+{
+    if (player.getPosition().x <= 7740 && player.getPosition().x >= 600)
+        view.setCenter(player.getPosition());
+    else if (player.getPosition().x > 7740) {
+        view.setCenter(7741, player.getPosition().y);
+    }
+    else if (player.getPosition().x < 600)
+        view.setCenter(599, player.getPosition().y);
+    
+}
+void Playersetup(Player& mc)
+{
+    
+    mc.Playertex.loadFromFile(resourcePath()+"Yellow Naruto.png");
+    mc.PlayerSprite.setTexture(mc.Playertex);
+    mc.PlayerSprite.setPosition(0, 0);
+    mc.PlayerSprite.setScale(5, 5);
+}
+bool canMoveRight(RectangleShape object,int xPoisition)
+{
+    if(object.getPosition().x < xPoisition)
+        return true;
+    
+    return false;
+    
+}
+
+bool canMoveleft(RectangleShape object,int xPoisition)
+{
+    if(object.getPosition().x > xPoisition)
+        return true;
+    
+    return false;
+    
 }
