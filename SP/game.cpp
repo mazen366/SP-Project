7bebe9ba9eef63feee3b0c72f99132c675895909
MazeN Hassn <mazenby6@gmail.com>
2023-04-11 22:21:33 +0200

    Level 1 B

--- SP/game.cpp
+++ SP/game.cpp
@@ -1,17 +1,15 @@
 #include <SFML/Graphics.hpp>
-#include <Windows.h>
 #include<thread>
 #include <iostream>
-
 using namespace std;
 using namespace sf;
 RenderWindow window(sf::VideoMode(1920, 1080), "Game");
 Event event;
 RectangleShape player, ground[100];
-Texture bgTexture;
-Sprite bgSprite;
-float bgCounter = 0;
-bool isBlackscreen = false;
+Texture bgTexture[30];
+Sprite bgSprite[30];
+int bgCounter = 0, leftx = -370, rightx = 8643;
+bool isBlackscreen = false, isMoved = false;
 Clock blackscreenTimer;
 View view(Vector2f(0, 0), Vector2f(1920, 1080));
 
@@ -39,46 +37,52 @@ int main()
     windowfunction();
     return 0;
 }
-
-
 //DEFINITIONS
 void bgSetup()
 {
-    int bgCounter = 0;
     player.setSize(Vector2f(50, 50));
     player.setPosition(600, 600);
     ground[0].setSize(Vector2f(9200, 30));
     ground[0].setPosition(-370, 800);
-    bgTexture.loadFromFile("Level 1-A BG.png");
-    bgSprite.setTexture(bgTexture);
-    bgSprite.setPosition(-370, -53);
+    bgTexture[0].loadFromFile("Level 1-A BG.png");
+    bgSprite[0].setTexture(bgTexture[0]);
+    bgSprite[0].setPosition(-370, -53);
+
+    ground[1].setSize(Vector2f(9200, 30));
+    ground[1].setPosition(-370, 800);
+    bgTexture[1].loadFromFile("Level 1-B-1 BG.png");
+    bgSprite[1].setTexture(bgTexture[1]);
+    bgSprite[1].setPosition(10000, 0);
+
 }
 void windowfunction()
 {
 
     while (window.isOpen())
     {
-        if (Keyboard::isKeyPressed(Keyboard::Key::Right) && canMoveRight(player, 8643))
+        if (Keyboard::isKeyPressed(Keyboard::Key::Right) && canMoveRight(player, rightx))
         {
             player.move(20, 0);
         }
-        if (Keyboard::isKeyPressed(Keyboard::Key::Left) && canMoveleft(player, -370))
+        if (Keyboard::isKeyPressed(Keyboard::Key::Left) && canMoveleft(player, leftx))
         {
             player.move(-20, 0);
         }
         cameraView();
         windowclose();
         window.clear();
-        window.draw(bgSprite);
+        window.draw(bgSprite[0]);
+        window.draw(bgSprite[1]);
         window.draw(player);
         // window.draw(mc.PlayerSprite);
         window.setView(view);
-        if (player.getPosition().x > 8633)
+        if (player.getPosition().x > 8633 && bgCounter == 0)
         {
             transition();
             transition_reverse();
-            player.setPosition(600, 600);
+            bgCounter = 1;
             this_thread::sleep_for(chrono::milliseconds(300));
+
         }
         else
             window.display();
@@ -97,22 +101,36 @@ void windowclose()
 }
 void cameraView()
 {
-    if (player.getPosition().x <= 7740 && player.getPosition().x >= 600)
-        view.setCenter(player.getPosition());
-    else if (player.getPosition().x > 7740) {
-        view.setCenter(7741, player.getPosition().y);
+    if (bgCounter == 0) {
+        if (player.getPosition().x <= 7740 && player.getPosition().x >= 600)
+            view.setCenter(player.getPosition());
+        else if (player.getPosition().x > 7740) {
+            view.setCenter(7741, player.getPosition().y);
+        }
+        else if (player.getPosition().x < 600)
+            view.setCenter(599, player.getPosition().y);
     }
-    else if (player.getPosition().x < 600)
-        view.setCenter(599, player.getPosition().y);
+    else if (bgCounter == 1) 
+    {
+        if (!isMoved)
+        {
+            player.setPosition(10400, 600);
+            isMoved = true;
+        }
+        leftx = 10000;
+        rightx = 14771;
+        //4771    1192
+        view.setCenter(player.getPosition());
+    }
 
 }
 void Playersetup(Player& mc)
 {
 
-    mc.Playertex.loadFromFile("Yellow Naruto.png");
-    mc.PlayerSprite.setTexture(mc.Playertex);
-    mc.PlayerSprite.setPosition(0, 0);
-    mc.PlayerSprite.setScale(5, 5);
+    // mc.Playertex.loadFromFile("Yellow Naruto.png");
+     //mc.PlayerSprite.setTexture(mc.Playertex);
+    // mc.PlayerSprite.setPosition(0, 0);
+    // mc.PlayerSprite.setScale(5, 5);
 }
 void transition()
 {
@@ -126,7 +144,7 @@ void transition()
             if (t1.getElapsedTime().asMilliseconds() > 300)
             {
                 t1.restart();
-                break;  
+                break;
             }
         }
     }
@@ -136,7 +154,7 @@ void transition_reverse()
     Clock t1;
     for (int i = 0; i < 4; i++)
     {
-        window.clear(Color{ Uint8(12 * i), Uint8(12 * i), Uint8(12 * i)});
+        window.clear(Color{ Uint8(12 * i), Uint8(12 * i), Uint8(12 * i) });
         window.display();
         while (true)
         {
@@ -150,15 +168,16 @@ void transition_reverse()
 }
 bool canMoveRight(RectangleShape object, int xPoisition)
 {
-    if (object.getPosition().x < xPoisition)
+    if (object.getPosition().x <= xPoisition)
         return true;
 
     return false;
 
 }
+
 bool canMoveleft(RectangleShape object, int xPoisition)
 {
-    if (object.getPosition().x > xPoisition)
+    if (object.getPosition().x >= xPoisition)
         return true;
 
     return false;
